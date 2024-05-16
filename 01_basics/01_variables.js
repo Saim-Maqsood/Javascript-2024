6a2bcad07909ebfdb2d6c61413acc327d2abe779
Saim Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2024-05-16 16:56:38 +0000

    Variables JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId = 144553; // Can not be changed
+let accountEmail = "maqsoodsaim2002@gmail.com"; // can be changed/redeclaired in local scope
+var password = "12345"; // Can be changed globally
+accountCity = "Lahore"; // not a preferred way to declair variables
+console.table([accountId,accountEmail,password,accountCity])
