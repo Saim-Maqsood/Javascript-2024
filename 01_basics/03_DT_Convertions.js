d400cb0d78d4be0f2a6de242225b9c75a3123cd3
Saim Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2024-05-19 10:08:36 +0000

    dataTypeConversions

---
+++ 01_basics/03_DT_Convertions.js
@@ -0,0 +1,37 @@
+// Converting string to number.
+
+// Converting number to string.
+
+score = 34
+stringScore = String(score)
+// console.log(typeof stringScore)
+
+// Converting string to number.
+
+age = "34"
+numberAge = Number(age)
+// console.log(typeof numberAge)
+
+/*
+34abc => result => NaN Type => number
+saim => NaN
+*/
+
+// ************** OPERATIONS **************************
+
+// Basic Operations
+
+/* 
+2+2
+2-2
+2/2
+2*2
+2**2
+etc...
+*/
+
+/* Challenges
+
+1. operation starting with string => no conversion
+2. operation ending with string => 2+2+"2" => 42
+*/
