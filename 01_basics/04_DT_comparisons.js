33b2d4a106b4dfbdc55054c32f40370ca72b529a
Saim Maqsood <97038010+Saim-Maqsood@users.noreply.github.com>
2024-05-22 18:54:48 +0000

    DT comparison

---
+++ 01_basics/04_DT_comparisons.js
@@ -0,0 +1,17 @@
+// Basic comparisons.
+/* 1. Greater than or equal to (>=)
+2. Less than or eqyal to (<=)
+3. greater than (>)
+4. Less tham (<)
+5. Not equal to (!=)
+6. Equal to (==)
+6. Strictly equal to (===)
+*/
+
+// ************************************ Complex/confusing examples *************************************
+// not recommmended examples
+
+console.log(+true)
+console.log(+"")
+
+// Note: Reason behind telling you these statements are not recommended is because it is not readable
